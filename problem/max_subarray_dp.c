a6353351d1b5660e54009fb57293e01381a57e83
carmenloklok <carmenloklok@gmail.com>
2013-09-19 10:19:27 +0800

    improve max subarry dp.

--- problem/max_subarray_dp.c
+++ problem/max_subarray_dp.c
@@ -1,26 +1,26 @@
-#define COUNT 17
+#define COUNT 16
 int main()
 {
-	int data[] = { -999999, 13, -3, -25, 20, -3, -16,
+	int data[] = { 0, 13, -3, -25, 20, -3, -16,
 		       -23, 18, 20, -7, 12, -5, -22,
 		       15, -4, 7 };
 
 	int result[COUNT];
 	memset(result, 0, COUNT);
-	for (int i = 1; i < COUNT; ++i)
+
+	int sum = 0;
+	int b = 0;
+	for (int i = 1; i <= COUNT; i++)
 	{
-		int max = result[i - 1];
-		for (int j = 1; j <= i; ++j)
-		{
-			int sum = 0;
-			for (int k = j; k <= i; ++k)
-				sum += data[k];
-			if (sum > max)
-				max = sum;
-		}
-		result[i] = max;
+		if (b > 0)
+			b += data[i];
+		else
+			b = data[i];
+		if (b > sum)
+			sum = b;
+		result[i] = sum;
 	}
 
-	for (int i = 1; i < COUNT; ++i)
+	for (int i = 1; i <= COUNT; ++i)
 		printf("%d: %d\n", i, result[i]);
 }
